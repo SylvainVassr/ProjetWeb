e73683cd0c9089eb32998cb0298441ce8fb570be
Sébastien Agnez <sebastien.agnez@ens.uvsq.fr>
2020-10-21 14:40:58 +0200

    correction bug

--- src/CatalogueApp/Controller/CatalogueController.php
+++ src/CatalogueApp/Controller/CatalogueController.php
@@ -43,45 +43,6 @@ class CatalogueController
     public function makeHomePage()
     {
         $title = "Catalogue de fichiers PDF";
-        /*$content = "<div style='text-align: center'>
-                        <div class='img-container'>
-                            <a href='#'>
-                            <img src='src/CatalogueApp/Model/img/all-document1.jpeg'>
-                            <div class='title'>Consequat</div>
-                            </a>
-                        </div>
-                        <div class='img-container'>
-                            <a href='#'>
-                            <img src='src/CatalogueApp/Model/img/all-document2.jpeg'>
-                            <div class='title'>Congue</div>
-                            </a>
-                        </div>
-                        <div class='img-container'>
-                            <a href='#'>
-                            <img src='src/CatalogueApp/Model/img/all-document3.jpeg'>
-                            <div class='title'>Mollis est</div>
-                            </a>
-                        </div>
-                        <div class='img-container'>
-                            <a href='#'>
-                            <img src='src/CatalogueApp/Model/img/all-document4.jpeg'>
-                            <div class='title'></div>
-                            </a>
-                        </div>
-                        <div class='img-container'>
-                            <a href='#'>
-                            <img src='src/CatalogueApp/Model/img/all-document5.jpeg'>
-                            <div class='title'></div>
-                            </a>
-                        </div>
-                        <div class='img-container'>
-                            <a href='#'>
-                            <img src='src/CatalogueApp/Model/img/all-document6.jpeg'>
-                            <div class='title'></div>
-                            </a>
-                        </div>
-                    </div>";*/
-
         $path = 'src/CatalogueApp/Model/img/';
         $directory = opendir($path);
 
@@ -89,7 +50,6 @@ class CatalogueController
         while($file = readdir($directory)) {
             if(!is_dir($path.$file))
             {
-                //echo '<a href="'.$path.$file.'">'.$file.'<br></a>';
                 $content .= "<div class='img-container'>
                                     <a href='$path$file'>
                                     <img src='$path$file'>
