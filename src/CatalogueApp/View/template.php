6effbe77bb0f29fc1ec7af8bc04536db4553f3f0
Sébastien AGNEZ <sebastien.agnez@orange.fr>
2020-10-21 11:04:32 +0200

    Style de la page modifié + boutons

--- src/CatalogueApp/View/template.php
+++ src/CatalogueApp/View/template.php
@@ -10,7 +10,16 @@
         <ul>
             <?php
             foreach ($menu as $text => $link) {
-                echo "<li><a href=\"$link\">$text</a></li>";
+                echo "<li><div class='position'>
+                            <div class='svg-wrapper'>
+                                <svg height='40' width='150' xmlns='http://www.w3.org/2000/svg'>
+                                <rect id='shape' height='40' width='150' />
+                                <div id='text'>
+                                    <a href=\"$link\"><span class='spot'></span>$text</a>
+                                </div>
+                                </svg>
+                            </div>
+                          </div>";
             }
             ?>
         </ul>
@@ -19,16 +28,44 @@
     if (!isset($_SESSION['user'])) {
         echo "<form method='post' action=''>
                                 <ul class='auth'>
-                                    <li><label>Login : </label><input name='login' type='text'></li>
-                                    <li><label>Mdp : </label><input name='mdp' type='password'></li>                             
-                                    <li><input type='submit' value='Connexion'></form></li>                                    
+                                    <li><div class='barre_auth'><label>Login : </label><input name='login' type='text'></div></li>
+                                    <li><div class='barre_auth'><label>Mdp : </label><input name='mdp' type='password'></div></li>                             
+                                    <li><div class='position'>
+                                            <div class='svg-wrapper'>
+                                                <svg height='40' width='150' xmlns='http://www.w3.org/2000/svg'>
+                                                <rect id='shape' height='40' width='150' />
+                                                <div id='text'>
+                                                    <input type='submit' value='Connexion'>  
+                                                </div>
+                                                </svg>
+                                            </div>
+                                        </div>
+                                   </form></li>                                    
                                 </ul>";
     } else {
         echo "<form method='post' action=''>
                     <ul class='auth'>
-                        <li><label>Bonjour, " . $_SESSION['user']["prenom"] . " " . $_SESSION['user']["nom"] . "</li>
-                        <li><a href='?objet=catalogue&amp;action=contenuUpload&amp;id=03'>Upload fichier</a></li>
-                        <li><input name='deconnexion' type='submit' value='Deconnexion'></form></li>
+                        <li><div class='barre_auth'><label>Bonjour, " . $_SESSION['user']["prenom"] . " " . $_SESSION['user']["nom"] . "</label></div></li>
+                        <li><div class='position'>
+                            <div class='svg-wrapper'>
+                                <svg height='40' width='150' xmlns='http://www.w3.org/2000/svg'>
+                                <rect id='shape' height='40' width='150' />
+                                <div id='text'>
+                                    <a href='?objet=catalogue&amp;action=contenuUpload&amp;id=03'><span class='spot'></span>Upload fichier</a>
+                                </div>
+                                </svg>
+                            </div>
+                          </div></li>
+                        <li><div class='position'>
+                                            <div class='svg-wrapper'>
+                                                <svg height='40' width='150' xmlns='http://www.w3.org/2000/svg'>
+                                                <rect id='shape' height='40' width='150' />
+                                                <div id='text'> 
+                                                    <input name='deconnexion' type='submit' value='Deconnexion'>                                                  
+                                                </div>
+                                                </svg>
+                                            </div>
+                                        </div></form></li>
                     </ul>";
         if (key_exists("deconnexion", $_POST)) {
             session_unset();
